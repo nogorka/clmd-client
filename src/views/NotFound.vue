da909cb0a2c069bc82291aaccc49cb87d10e068e
Ekaterina Chernogor <chernogor_k@mail.ru>
2024-04-28 19:24:30 +0300

    add icons

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -3,9 +3,11 @@
   <div>
     <p>Sorry</p>
     <p>Requested content not found. Would you like to go home?</p>
-    <div class="flex">
-      <!--      <span class="pi pi-user"></span>-->
-      <router-link to="/" class="text-blue-600 visited:text-purple-600"> YES </router-link>
+    <div class="flex gap-2 py-2 vertical-mid">
+      <router-link to="/" class="text-blue-600 visited:text-purple-600">
+        <span class="pi pi-arrow-left align-mid"> YES</span>
+      </router-link>
     </div>
   </div>
 </template>
+<script setup lang="ts"></script>
