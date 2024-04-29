f933e541c3a786c1a76100e311d99d2f4102e156
Ekaterina Chernogor <chernogor_k@mail.ru>
2024-04-29 12:25:36 +0300

    dependencies: add ElementPlus

--- src/main.js
+++ src/main.js
@@ -7,8 +7,12 @@ import router from './router'
 import 'leaflet/dist/leaflet.css'
 import 'primeicons/primeicons.css'
 
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
 const app = createApp(App)
 
 app.use(router)
+app.use(ElementPlus)
 
 app.mount('#app')
