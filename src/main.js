b647b9eb092d6f42b10f6868f738aa2359f4d06d
Ekaterina Chernogor <chernogor_k@mail.ru>
2024-04-29 16:20:33 +0300

    feature: add vuex

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
-import './assets/main.css'
+import '@/assets/main.css'
 
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import App from '@/App.vue'
+import router from '@/router'
+import store from '@/store/index.js'
 
 import 'leaflet/dist/leaflet.css'
 import 'primeicons/primeicons.css'
@@ -13,6 +14,7 @@ import 'element-plus/dist/index.css'
 const app = createApp(App)
 
 app.use(router)
+app.use(store)
 app.use(ElementPlus)
 
 app.mount('#app')
