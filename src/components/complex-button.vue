4fa7adcfd3dd98df3c290ed9e70f9a89647b4557
Ekaterina Chernogor <chernogor_k@mail.ru>
2024-04-29 12:36:38 +0300

    feature: welcome page

---
+++ src/components/complex-button.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex gap-2">
+    <span :class="props.icon"></span>
+    <router-link :to="props.to" class="btn"> <slot /></router-link>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  icon: {
+    type: String,
+    default: ''
+  },
+  to: {
+    type: String,
+    default: '/'
+  }
+})
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,14 @@
-<script setup></script>
+<script setup>
+import ComplexButton from '@/components/complex-button.vue'
+</script>
 
 <template>
-  <div>APP</div>
+  <div class="container mx-auto p-4">
+    <h1 class="text-center text-2xl font-bold my-4">Welcome! Choose an input method:</h1>
+    <div class="flex justify-center gap-4">
+      <complex-button icon="pi pi-table" to="/import-file">Import File</complex-button>
+      <complex-button icon="pi pi-search" to="/manual-input">Input Manually</complex-button>
+      <complex-button icon="pi pi-qrcode" to="/scan-qr">Scan QR Code</complex-button>
+    </div>
+  </div>
 </template>
