db96686258dd1501abc81ac42c1805fdbd180e8b
Ekaterina Chernogor <chernogor_k@mail.ru>
2024-04-30 13:41:19 +0300

    feature: add optimize-route-button

---
+++ src/components/optimize-route-button.vue
@@ -0,0 +1,9 @@
+<template>
+  <button
+    type="button"
+    class="mt-4 w-full border-2 border-blue-500 text-blue-500 py-2 rounded hover:bg-blue-500 hover:text-white focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+  >
+    Optimize route
+    <i class="pi pi-truck ml-2"></i>
+  </button>
+</template>
--- src/views/FileImport.vue
+++ src/views/FileImport.vue
@@ -1,6 +1,7 @@
 <script setup>
 import GoBack from '@/components/go-back.vue'
 import { processCsv, processJson } from '@/utils/file.js'
+import OptimizeRouteButton from '@/components/optimize-route-button.vue'
 
 
 const onChange = (e) => {
@@ -29,15 +30,18 @@ const onChange = (e) => {
 </script>
 
 <template>
-  <div class="container mx-auto p-4">
+  <div class="container mx-auto p-4 ">
     <go-back />
+    <div class="max-w-lg mx-auto">
+      <h1 class="text-center text-2xl font-bold my-4">File Import</h1>
+      <input
+        class="block w-full text-center p-2 mx-auto my-4 border-2 border-gray-300 rounded-lg cursor-pointer hover:border-blue-500 focus:border-blue-500"
+        type='file'
+        accept=".json, .csv"
+        @change="onChange"
+      />
 
-    <h1 class="text-center text-2xl font-bold my-4">File Import</h1>
-    <input
-      class="block mx-auto my-4"
-      type='file'
-      accept=".json, .csv"
-      @change="onChange"
-    />
+      <optimize-route-button />
+    </div>
   </div>
 </template>
